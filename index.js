7bb4d62f058ad60eb0e427ccd32ea580635a5f5b
FIAP <FIAP>
2019-10-08 22:38:56 -0300

    ttt

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/users', (request, response, next) => {
+    //console.log(request.query.id);    
+    //console.log(response);
+    console.log("Entrei na rota USERS")
+    response.json({ success: true });
+})
+
+app.listen(port, () => {
+    console.log(`App Listening on port ${port}`);
+});
+
 console.log('Meu Ambiente', process.env.NODE_ENV);
